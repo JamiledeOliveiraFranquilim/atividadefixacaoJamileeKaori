02593b75cc606d4efe00242b73b64798dfb5040c
kaorishi11 <kaorishimada11@gmail.com>
2026-07-31 11:46:56 -0300

    supa

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://xxfyrxycbnnrizxoexre.supabase.co";
+const SUPABASE_KEY = "sb_publishable_2P7CdAPFH68FhLosaNi1-A_2O-Hj7_n";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
